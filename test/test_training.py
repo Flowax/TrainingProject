ca3074e3fea45bb4be069b400112e096c56e94b1
George <Flowax99@gmail.com>
2025-07-18 23:32:20 +0300

    trainingProject v2.0

--- test/test_training.py
+++ test/test_training.py
@@ -1,39 +1,47 @@
-from playwright.sync_api import sync_playwright, expect
 import allure
-import pytest
+
+from config.utils.reporter import ReportPage
 
 from page.base_page import BASE_URL
 
 @allure.epic("Тренировка автотестов")
-class TestSmoke:
+class TestSaucedemo:
 
     @allure.title("Тест SAUCEDEMO")
-    def test_saucedemo(self):
-        playwright = sync_playwright().start()
-        browser = playwright.firefox.launch(headless=False)
-        page = browser.new_page()
+    def test_saucedemo(self, page):
+        report_page = ReportPage(page)
+        # Открываем страницу, проверяем тайтл
+        page.goto(BASE_URL, timeout=10000)
+        with allure.step("Проверка открытия страницы"):
+            try:
+                assert page.title() == "Swag Labs"
+            except AssertionError:
+                report_page.attach_screenshot()
+                raise
 
-        try:
-            # Открываем страницу, проверяем тайтл
-            page.goto(BASE_URL, timeout=10000)
-            expect(page).to_have_title("Swag Labs")
+        # Авторизация
+        page.fill("#user-name", "standard_user")
+        page.fill("#password", "secret_sauce")
+        page.click("#login-button")
 
-            # Авторизация
-            page.fill("#user-name", "standard_user")
-            page.fill("#password", "secret_sauce")
-            page.click("#login-button")
+        # Проверка авторизации
+        with allure.step("Проверка авторизации"):
+            try:
+                assert page.locator("#header_container > div.header_secondary_container > span").is_visible()
+                assert page.title() == "Swag Lab"
+            except AssertionError:
+                report_page.attach_screenshot()
+                raise
 
-            # Проверка авторизации
-            assert page.locator("#header_container > div.header_secondary_container > span").is_visible()
-            assert page.title() == "Swag Labs"
-            # Добавление товара в корзину
-            page.click("#item_4_title_link > div")
-            page.click("#add-to-cart")
+        # Добавление товара в корзину
+        page.click("#item_4_title_link > div")
+        page.click("#add-to-cart")
 
-            # Проверка добавления товара в корзину
-            assert page.locator("#remove").is_visible()
-
-        finally:
-            # Закрытие
-            browser.close()
-            playwright.stop()
+        # Проверка добавления товара в корзину
+        with allure.step("Проверка добавления товара в корзину"):
+            try:
+                assert page.locator("#remove").is_visible()
+                report_page.attach_screenshot()
+            except AssertionError:
+                report_page.attach_screenshot()
+                raise
